d732805fa90912dc1d30b0b0aa5f846656a7c5ff
fajarriv <rivaldiibnusina@gmail.com>
2025-05-07 13:03:32 +0700

    setup rabbitmq

--- app/main.py
+++ app/main.py
@@ -1,16 +1,80 @@
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from app.api.routes import routes
 from app.api.websockets import websocket
+from app.rmq import PikaClient
+import threading
+import asyncio
+import logging
+import logging.config
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+
+logging_config = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'myapp': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger('myapp')
+
+
+def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
+    # inspired from https://gist.github.com/dmfigol/3e7d5b84a16d076df02baa9f53271058
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # init rmq consumer when app starts
+    logger.critical("Starting RMQ consumer")
+
+    # attach rmq consumer to app
+    app.rmq_consumer = PikaClient(queue_name="ids-queue", host="rabbitmq",
+                                  port=5672, user="guest", password="guest")
+    app.consumer_loop = asyncio.new_event_loop()
+    tloop = threading.Thread(target=start_background_loop, args=(
+        app.consumer_loop,), daemon=True)
+    tloop.start()
+
+    _ = asyncio.run_coroutine_threadsafe(
+        app.rmq_consumer.start_consumer(), app.consumer_loop)
+
+    yield
+    # shutdown event
+    await app.rmq_consumer.disconnect()
+    app.consumer_loop.stop()
+
+
+app = FastAPI(title="Detection Engine Module", debug=True, lifespan=lifespan)
 
 app.include_router(routes.router)
 app.include_router(websocket.router)
+
 
 @app.get("/")
 async def root():
     return {"message": "IDS Backend is running."}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ app/rmq.py
@@ -0,0 +1,71 @@
+import logging
+import aio_pika
+import asyncio
+
+
+logger = logging.getLogger("myapp")
+
+
+class PikaClient:
+
+    def __init__(self, queue_name: str, host: str, port: int, user: str, password: str):
+        self.queue_name = queue_name
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+
+        self.connection: aio_pika.RobustConnection = None
+        self.channel: aio_pika.abc.AbstractChannel = None
+        self.queue = None
+        self.packet_counter = 0
+
+    async def start_connection(self):
+        try:
+            logger.info("Starting a new connection")
+            self.connection = await aio_pika.connect_robust(host=self.host, port=self.port, login=self.user, password=self.password)
+
+            logger.info("Opening channel")
+            self.channel = await self.connection.channel()
+
+            await self.setup_queue()
+        except Exception as e:
+            logger.error(e)
+
+    async def setup_queue(self):
+        logger.info("Setup a queue: %s" % self.queue_name)
+        self.queue = await self.channel.declare_queue(name=self.queue_name, durable=True)
+
+    async def start_consumer(self):
+        await self.start_connection()
+
+        await self.channel.set_qos(prefetch_count=1)
+
+        logger.info("Starting consumer")
+        try:
+            await self.queue.consume(self.handle_message, no_ack=False)
+        except Exception as _e:
+            print(_e)
+            logger.error(_e)
+        print("here")
+        logger.info("Consumer has been started")
+        return self
+
+    async def handle_message(self, message: aio_pika.abc.AbstractIncomingMessage):
+        """Handle incoming message"""
+
+        # TODO: pass to model and broadcast
+        # await asyncio.sleep(3)
+        self.packet_counter += 1
+        logger.info("total packets recieved: %s" % self.packet_counter)
+
+        # manual ack mechanism to tell broker that the message has been processed properly
+        await message.ack()
+
+
+    async def disconnect(self):
+        try:
+            if not self.connection.is_closed:
+                await self.connection.close()
+        except Exception as _e:
+            logger.error(_e)
