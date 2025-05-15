e603d35e9742b4d7dfb4801580199ac585a0f193
alifismady <alifismady@gmail.com>
2025-05-15 21:11:20 +0700

    adjust model path

--- app/models/model.py
+++ app/models/model.py
@@ -6,15 +6,18 @@ import joblib
 from app.preprocessing.preprocessing import preprocess_data
 
 # Multi-Class
-model_path = os.path.join(os.path.dirname(__file__), '../../trained_models/dnn/1305/model.h5')
-label_encoder_path = os.path.join(os.path.dirname(__file__), '../../trained_models/dnn/1305/label_encoder.pkl')
+model_path = os.path.join(os.path.dirname(__file__), '../../trained_models/cnn/1505_26/model.h5')
+label_encoder_path = os.path.join(os.path.dirname(__file__), '../../trained_models/cnn/1505_26/label_encoder.pkl')
 model = tf.keras.models.load_model(model_path)
 label_encoder = joblib.load(label_encoder_path) 
 
 
 def predict(data_list):
     processed_features = preprocess_data(data_list)
-    predictions = model.predict(processed_features)
+    # For CNN and RNN
+    processed_features_rnncnn = processed_features.reshape(processed_features.shape[0], 1, processed_features.shape[1])
+    predictions = model.predict(processed_features_rnncnn)
+    # predictions = model.predict(processed_features)
     predicted_class_indices = np.argmax(predictions, axis=1)
     confidences = np.max(predictions, axis=1)
     
@@ -27,4 +30,5 @@ def predict(data_list):
             'confidence': float(confidence)
         })
     
+
     return results
--- app/preprocessing/preprocessing.py
+++ app/preprocessing/preprocessing.py
@@ -9,24 +9,22 @@ PREDEFINED_SERVICES = {
 }
 
 # Based on first reference paper
-# This will not be used when new trained model uploaded
-# SELECTED_FEATURES = [
-#     'duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment', 'urgent',
-#     'count', 'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate', 'same_srv_rate',
-#     'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate',
-#     'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
-#     'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate'
-# ]
+SELECTED_FEATURES = [
+    'duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment', 'urgent',
+    'count', 'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate', 'same_srv_rate',
+    'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate',
+    'dst_host_diff_srv_rate', 'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
+    'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate'
+]
 
 # Multi-Class
-preprocessor_path = os.path.join(os.path.dirname(__file__), '../../trained_models/dnn/1305/preprocessor.joblib')
+preprocessor_path = os.path.join(os.path.dirname(__file__), '../../trained_models/cnn/1505_26/preprocessor.joblib')
 preprocessor = joblib.load(preprocessor_path)
 
 
 def preprocess_data(data_list):
     """Process a batch of network feature dictionaries"""
-    # This [SELECTED_FEATURES] is temporary for the current trained model
-    df = pd.DataFrame(data_list)
+    df = pd.DataFrame(data_list)[SELECTED_FEATURES]
     
     df['service'] = df['service'].apply(
         lambda x: x if x in PREDEFINED_SERVICES else 'other'
