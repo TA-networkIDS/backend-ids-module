7e24baa81a77aa9b2384c7d32bec390a73d51135
alifismady <alifismady@gmail.com>
2025-05-18 21:30:11 +0700

    delete all packet ws implentation

--- app/api/websockets/ws.py
+++ app/api/websockets/ws.py
@@ -9,7 +9,6 @@ logger = logging.getLogger("websocket")
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
-        self.all_traffic_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
         """
@@ -19,14 +18,6 @@ class ConnectionManager:
         self.active_connections.append(websocket)
         logger.info(f"New WebSocket connection. Total connections: {len(self.active_connections)}")
 
-    async def connect_all(self, websocket: WebSocket):
-        """
-        Connect a new WebSocket connection for all traffic
-        """
-        await websocket.accept()
-        self.all_traffic_connections.append(websocket)
-        logger.info(f"New WebSocket connection. Total connections: {len(self.all_traffic_connections)}")
-
     def disconnect(self, websocket: WebSocket):
         """
         Disconnect a WebSocket connection
@@ -34,28 +25,11 @@ class ConnectionManager:
         self.active_connections.remove(websocket)
         logger.info(f"WebSocket disconnected. Remaining connections: {len(self.active_connections)}")
 
-    def disconnect_all(self, websocket: WebSocket):
-        """
-        Disconnect a WebSocket connection for all traffic
-        """
-        self.all_traffic_connections.remove(websocket)
-        logger.info(f"WebSocket disconnected. Remaining connections: {len(self.all_traffic_connections)}")
-
     async def broadcast(self, message: str):
         """
         Broadcast a message to all connections
         """
         for connection in self.active_connections:
-            try:
-                await connection.send_json(message)
-            except Exception as e:
-                logger.error(f"Error broadcasting message: {e}")
-
-    async def broadcast_all(self, message: str):
-        """
-        Broadcast a message to all connections for all traffic
-        """
-        for connection in self.all_traffic_connections:
             try:
                 await connection.send_json(message)
             except Exception as e:
@@ -73,18 +47,5 @@ async def websocket_endpoint(websocket: WebSocket):
             await websocket.receive_text()
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-
-@router.websocket("/ws/all")
-async def websocket_endpoint(websocket: WebSocket):
-    """
-    WebSocket endpoint for IDS alerts and updates but include all packets
-    """
-    await manager.connect_all(websocket)
-    try:
-        while True:
-            # Keep connection open, but don't expect client to send messages
-            await websocket.receive_text()
-    except WebSocketDisconnect:
-        manager.disconnect_all(websocket)
 
 manager = ConnectionManager()
